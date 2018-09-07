85a4b05decdcf248b9c3ec7170924ac2c4c33a0f
Cristian Gonzalez <cgonzalez@sistran.com.co>
2018-09-06 21:51:27 -0500

    cambio en el login

--- CapaPresentacion/App_Start/RouteConfig.cs
+++ CapaPresentacion/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace CapaPresentacion
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
+                //defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
