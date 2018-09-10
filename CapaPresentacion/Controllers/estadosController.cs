c97f413826dd87cddb6e3daf2a32caaf9930e43f
Nelson Perilla <nelsonperilla@misena.edu.co>
2018-09-09 19:21:27 -0500

    Creacion del modelo.edmx, controladores y vistas

---
+++ CapaPresentacion/Controllers/estadosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CapaPresentacion.Models;
+
+namespace CapaPresentacion.Controllers
+{
+    public class estadosController : Controller
+    {
+        private colegioEntities db = new colegioEntities();
+
+        // GET: estados
+        public ActionResult Index()
+        {
+            return View(db.estados.ToList());
+        }
+
+        // GET: estados/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            estados estados = db.estados.Find(id);
+            if (estados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estados);
+        }
+
+        // GET: estados/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: estados/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,descripcion")] estados estados)
+        {
+            if (ModelState.IsValid)
+            {
+                db.estados.Add(estados);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(estados);
+        }
+
+        // GET: estados/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            estados estados = db.estados.Find(id);
+            if (estados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estados);
+        }
+
+        // POST: estados/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,descripcion")] estados estados)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(estados).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(estados);
+        }
+
+        // GET: estados/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            estados estados = db.estados.Find(id);
+            if (estados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estados);
+        }
+
+        // POST: estados/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            estados estados = db.estados.Find(id);
+            db.estados.Remove(estados);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
