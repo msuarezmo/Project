04e889b0c8eaf7a7ef40ca0a8d3b4ab567a35478
SISTRAN\msuarez <msuarez@sistran.com.co>
2018-09-10 16:46:52 -0500

    Módulo citas

---
+++ CapaPresentacion/Controllers/AppointmentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapaPresentacion.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        // GET: Appointment
+        public ActionResult Appointment()
+        {
+            return View();
+        }
+    }
+}
