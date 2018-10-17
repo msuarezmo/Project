58f15b20608c1ae0aca4dfc843dc005a38378cf5
SISTRAN\cgonzalez <cgonzalez@sistran.com.co>
2018-10-17 14:27:59 -0500

    Cambio en arquitectura, implementacion de bundles para el diseño

--- CapaPresentacion/App_Start/BundleConfig.cs
+++ CapaPresentacion/App_Start/BundleConfig.cs
@@ -13,6 +13,8 @@ namespace CapaPresentacion
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+            bundles.Add(new StyleBundle("~/Content/login").Include(
+          "~/Resources/css/login.css"));
 
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
             // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
